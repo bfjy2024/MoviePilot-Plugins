846df5eafe3538d2eacf6b28c55d52ea9faaf053
bfjy2024 <slwu19@163.com>
2026-01-30 10:27:48 +0800

    修改 qywechatbug

--- plugins/qywechatmsg/__init__.py
+++ plugins/qywechatmsg/__init__.py
@@ -15,7 +15,7 @@ class qyWechatMsg(_PluginBase):
     # 插件图标
     plugin_icon = "Wecom_A.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "bfjy"
     # 作者主页
