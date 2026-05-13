5ae5e5f8e17a61032f1507e36872b6c6d62bcb40
bfjy2024 <slwu19@163.com>
2026-05-13 16:00:28 +0800

    YzYY插件修改

--- plugins.v2/yzyysignin/__init__.py
+++ plugins.v2/yzyysignin/__init__.py
@@ -23,7 +23,7 @@ class YzyySignin(_PluginBase):
     # 插件图标
     plugin_icon = "yzyyA.png"
     # 插件版本
-    plugin_version = "1.0.0"
+    plugin_version = "1.0.1"
     # 插件作者
     plugin_author = "bfjy"
     # 作者主页
