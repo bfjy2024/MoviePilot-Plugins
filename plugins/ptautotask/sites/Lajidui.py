7867b932623fb7e7e3d1f93f83364414d6e88c2a
bfjy2024 <slwu19@163.com>
2026-05-22 17:00:57 +0800

    ljd

--- plugins/ptautotask/sites/Lajidui.py
+++ plugins/ptautotask/sites/Lajidui.py
@@ -3,22 +3,22 @@ from ..base.Decorator import task_info
 from ..base.BaseTask import BaseTask
 
 
-class NovaHD(NexusPHP):
+class Lajidui(NexusPHP):
 
     def __init__(self, cookie):
         super().__init__(cookie)
 
     @staticmethod
     def get_site_name():
-        return "NovaHD"
+        return "垃圾堆"
 
     @staticmethod
     def get_url():
-        return "https://pt.novahd.top"
+        return "https://pt.lajidui.top"
 
     @staticmethod
     def get_site_domain():
-        return "pt.novahd.top"
+        return "pt.lajidui.top"
 
     def claim_task(self, task_id: str, rt_method=None):
         return super().claim_task(task_id, lambda response: response.json().get("msg", "未知错误"))
@@ -26,11 +26,11 @@ class NovaHD(NexusPHP):
 
 class Tasks(BaseTask):
     def __init__(self, cookie: str):
-        super().__init__(NovaHD(cookie))
+        super().__init__(Lajidui(cookie))
 
-    @task_info(label="NovaHD每月任务领取", hint="领取NovaHD站点的保种任务")
-    def daily_claim_task(self):
-        task_id_list = ["3"]
+    @task_info(label="垃圾堆每月任务领取", hint="领取垃圾堆站点的每月保种计划任务")
+    def monthly_claim_task(self):
+        task_id_list = ["1"]
         return "\n".join([self.client.claim_task(item) for item in task_id_list])
 
     def daily_checkin(self):
