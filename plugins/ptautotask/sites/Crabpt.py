0969f86b7c61c0202978066392087a51cb1a95d2
bfjy2024 <slwu19@163.com>
2026-06-01 11:51:06 +0800

    蟹黄堡力争全勤

--- plugins/ptautotask/sites/Crabpt.py
+++ plugins/ptautotask/sites/Crabpt.py
@@ -32,6 +32,10 @@ class Tasks(BaseTask):
     def daily_claim_task(self):
         task_id_list = ["12"]
         return "\n".join([self.client.claim_task(item) for item in task_id_list])
-
+    
+    @task_info(label="蟹黄堡任务领取", hint="领取蟹黄堡站点的力争全勤任务")
+    def daily_claim_task(self):
+        task_id_list = ["11"]
+        return "\n".join([self.client.claim_task(item) for item in task_id_list])
     def daily_checkin(self):
         return self.client.attendance()
